a51f9734ee36e2447f9429a48ccc8d39327d0021
bswlkh0310 <hhhello0507@gmail.com>
2024-06-19 08:26:26 +0900

    UIControl+addAction

---
+++ Sources/MyUIKitExt/Extentions/UIControl/UIControl+addAction.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+public extension UIControl {
+    func addAction(for controlEvents: UIControl.Event = .touchUpInside, _ closure: @escaping () -> ()) {
+        @objc class ClosureSleeve: NSObject {
+            let closure: () -> ()
+            
+            init(_ closure: @escaping () -> ()) {
+                self.closure = closure
+            }
+            
+            @objc func invoke() {
+                closure()
+            }
+        }
+        
+        let sleeve = ClosureSleeve(closure)
+        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+        objc_setAssociatedObject(self, "\(UUID())", sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+}
